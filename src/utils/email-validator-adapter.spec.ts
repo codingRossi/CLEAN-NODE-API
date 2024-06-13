13c76dde9421d65f0400c41c70919e75d4d0be3b
codingrossi <codingrossi@gmail.com>
2024-06-13 18:35:07 +0300

    test: ensure  DbAddAccount call Encrypter with correct password

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,5 +1,5 @@
 import isEmail from "validator/lib/isEmail"
-import { EmailValidatorAdapter } from "./email.validator"
+import { EmailValidatorAdapter } from "./email.validator-adapter"
 import validator = require("validator")
 
 jest.mock('validator', () => ({
