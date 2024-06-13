d828d5ea2570c7140500cae9b2db2b3d26773a39
codingrossi <codingrossi@gmail.com>
2024-06-13 17:51:02 +0300

    refactor: change file name

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -21,13 +21,13 @@ describe('EmailValidator Adapter', () => {
     })
 
     test('Should return true if validator returns true', () => {
-        const sut = new EmailValidatorAdapter()  
+        const sut = makeSut()  
         const isValid = sut.isValid('valid_email@email.com')
         expect(isValid).toBe(true)
     })
 
     test('Should call validator with correct email', () => {
-        const sut = new EmailValidatorAdapter()  
+        const sut = makeSut()  
         const isEmailSpy = jest.spyOn(validator, 'isEmail')
         sut.isValid('valid_email@email.com')
         expect(isEmailSpy).toHaveBeenCalledWith('valid_email@email.com')
