a7d7c01d9cfaacc11e96cdd1b76fe0a8dd09ad89
Pedro Rossi <codingrossi@gmail.com>
2024-07-26 16:12:11 -0300

    test: ensure DbAddAccount throws if Encrypter throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,10 +1,5 @@
-import { Encrypter } from "../../protocols/encrypter"
-import { DbAddAccount } from "./de-add-account"
-
-interface SutTypes {
-    sut: DbAddAccount
-    encrypterStub: Encrypter
-}
+import { Encrypter, AddAccountModel, AccountModel, AddAccountRepository } from "./db-add-account-protocols"
+import { DbAddAccount } from "./db-add-account"
 
 const makeEncrypter = (): Encrypter => {
     class EncrypterStub implements Encrypter {
@@ -15,12 +10,35 @@ const makeEncrypter = (): Encrypter => {
     return new EncrypterStub()
 }
 
+const makeAddAccountRepository = (): AddAccountRepository => {
+    class AddAccountRepositoryStub implements AddAccountRepository {
+        async add(accountData: AddAccountModel): Promise<AccountModel> {
+            const fakeAccount = {
+                id: 'valid_id',
+                name: 'valid_name',
+                email: 'valid_email',
+                password: 'hashed_password'
+            }
+            return new Promise(resolve => resolve(fakeAccount))
+        }
+    }
+    return new AddAccountRepositoryStub()
+}
+
+interface SutTypes {
+    sut: DbAddAccount
+    encrypterStub: Encrypter
+    addAccountRepositoryStub: AddAccountRepository
+} 
+
 const makeSut = (): SutTypes => {
-    const encrypterStub = new makeEncrypter()
-    const sut = new DbAddAccount(encrypterStub)
+    const encrypterStub = makeEncrypter()
+    const addAccountRepositoryStub = makeAddAccountRepository()
+    const sut = new DbAddAccount(encrypterStub, addAccountRepositoryStub)
     return {
         sut,
-        encrypterStub
+        encrypterStub,
+        addAccountRepositoryStub
     }
 }
 
@@ -35,5 +53,33 @@ describe('DbAddAccount Usecase', () => {
         }
         await sut.add(accountdata)
         expect(encryptSpy).toHaveBeenCalledWith('valid_password')
+    })
+
+    test('Should throw if Encrypter throws', async () => {
+        const { sut, encrypterStub } = makeSut()
+        jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+        const accountData = {
+            name: 'valid_name',
+            email: 'valid_email',
+            password: 'valid_password'
+        }
+        const promise = sut.add(accountData)
+        await expect(promise).rejects.toThrow()
+    })
+
+    test('Should call AddAccountRepository with correct values', async () => {
+        const { sut, addAccountRepositoryStub } = makeSut()
+        const addSpy = jest.spyOn(addAccountRepositoryStub, 'add')
+        const accountdata = {
+            name: 'valid_name',
+            email: 'valid_email',
+            password: 'valid_password'
+        }
+        await sut.add(accountdata)
+        expect(addSpy).toHaveBeenCalledWith({
+            name: 'valid_name',
+            email: 'valid_email',
+            password: 'hashed_password'
+        })
     })
 })
---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,19 @@
+import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
+import { AccountModel } from '../../../domain/models/account'
+import { Encrypter } from '../../protocols/encrypter'
+import { AddAccountRepository } from './db-add-account-protocols'
+
+export class DbAddAccount implements AddAccount {
+    private readonly encrypter: Encrypter
+    private readonly addAccountRepository: AddAccountRepository
+
+    constructor(encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
+        this.encrypter = encrypter
+        this.addAccountRepository = addAccountRepository
+    }
+    async add (accountData: AddAccountModel): Promise<AccountModel> {
+        const hashedPassword = await this.encrypter.encrypt(accountData.password)
+        await this.addAccountRepository.add(Object.assign(accountData, { password: hashedPassword}))
+        return new Promise(resolve => resolve(null))
+    }
+}
