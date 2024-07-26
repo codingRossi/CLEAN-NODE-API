df388f813f551201b7bea233e57c9226c6d1fdf7
Pedro Rossi <codingrossi@gmail.com>
2024-07-26 16:17:44 -0300

    group all protocols to a single file

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,7 +1,5 @@
-import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
-import { AccountModel } from '../../../domain/models/account'
-import { Encrypter } from '../../protocols/encrypter'
-import { AddAccountRepository } from './db-add-account-protocols'
+import { AddAccount, AddAccountModel, AccountModel, Encrypter } from './db-add-account-protocols'
+
 
 export class DbAddAccount implements AddAccount {
     private readonly encrypter: Encrypter
