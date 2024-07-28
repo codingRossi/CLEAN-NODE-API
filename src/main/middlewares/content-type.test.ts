cabd4e4468815c5cb2fccf334be6db4415fca7b3
Pedro Rossi <codingrossi@gmail.com>
2024-07-28 18:02:37 -0300

    test: ensure SignUp route returns an account on success

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -3,21 +3,21 @@ import app from "../config/app"
 
 describe('Content Type Middleware', () => {
     test('Should return default content type as json', async () => {
-        app.post('/test_content_type', (req, res) => {
+        app.get('/test_content_type', (req, res) => {
             res.send('')
         })
         await request(app)
-        .get('/test_constent_type')
-        .expect('constent-type', /json/)
+        .get('/test_content_type')
+        .expect('content-type', /json/)
     })
 
     test('Should return xml content type when forced', async () => {
-        app.post('/test_content_type_xml', (req, res) => {
+        app.get('/test_content_type_xml', (req, res) => {
             res.type('xml')
             res.send('')
         })
         await request(app)
-        .get('/test_constent_type')
-        .expect('constent-type', /xml/)
+        .get('/test_content_type_xml')
+        .expect('content-type', /xml/)
     })
 })
