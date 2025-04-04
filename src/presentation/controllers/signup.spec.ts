b8891420f9fbad51c6caeadc3a032b33d4406527
codingRossi <codingrossi@gmail.com>
2025-04-04 19:34:00 -0300

    refactor: move sut creation to a helper makeSut factory

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -61,4 +61,19 @@ describe("Signup Controller", () => {
     expect(httpResponse.body).toEqual(new MissingParamError("passwordConfirmation"))
   })
 
+  test("Should return 400 if an invalid email is provided", () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "invalid_password",
+        password: "any_passoword",
+        passwordConfirmation: "any_password"
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError("passwordConfirmation"))
+  })
+
 })
