e54b909a60ca5a571d31f18a1108aee315e50f82
codingRossi <codingrossi@gmail.com>
2025-04-04 19:03:38 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/helper/httpHelper.ts
+++ src/presentation/helper/httpHelper.ts
@@ -1,9 +1,7 @@
 import { MissingParamError } from "../errors/missingParamError"
 import { HttpResponse } from "../protocols/http"
 
-export const badRequest = (error: Error): HttpResponse => {
-  return {
-    statusCode: 400,
-    body: error
-  }
-}
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
