d89b8c92ee722ffd74a1176d213cdf6413a846a3
codingRossi <codingrossi@gmail.com>
2025-04-07 15:43:57 -0300

    feat: ensure request return json as default content type

--- src/main/middlwares/content-type.test.ts
+++ src/main/middlwares/content-type.test.ts
@@ -3,11 +3,21 @@ import app from "../config/app"
 
 describe("Content type Middleware", () => {
   test("Should return as default content type as json", async () => {
-    app.post("/test_content_type", (req, res) => {
+    app.get("/test_content_type", (req, res) => {
       res.send("")
     })
     await request(app)
       .get('/test_content_type')
       .expect("content-type", /json/)
+  })
+
+  test("Should return xml content type when forced", async () => {
+    app.get("/test_content_type_xml", (req, res) => {
+      res.type("xml")
+      res.send("")
+    })
+    await request(app)
+      .get('/test_content_type_xml')
+      .expect("content-type", /xml/)
   })
 })
