549aadc69e55619b84853c47bf416157a18bd91c
codingRossi <codingrossi@gmail.com>
2025-04-07 15:37:24 -0300

    feat: ensure cors is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from "express"
 import { bodyParser } from "../middlwares/body-parser"
 import { cors } from "../middlwares/cors"
+import { contentType } from "../middlwares/content-type"
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }
