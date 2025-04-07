d88a2583fa2849478a7e84aae6476c34469cb33c
codingRossi <codingrossi@gmail.com>
2025-04-07 14:26:35 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,4 @@
-import { MongoClient } from "mongodb"
-import { disconnect } from "process"
+import { Collection, MongoClient } from "mongodb"
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -10,5 +9,9 @@ export const MongoHelper = {
 
   async disconnect(): Promise<void> {
     await this.client.close()
+  },
+
+  getColletion(name: string): Collection {
+    return this.client.db().collection(name)
   }
 }
