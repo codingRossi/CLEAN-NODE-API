3d17e955fc0c72364a34039c594943bf0d9b10fb
codingRossi <codingrossi@gmail.com>
2025-04-07 16:08:52 -0300

    feat: ensure Signup route returns an account on success

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,7 +1,6 @@
 import { Express } from "express"
 import { bodyParser, contentType, cors } from "../middlwares"
 
-
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
