90d216305e2190d44b10ce0c7ef5a357a33bbcec
codingrossi <codingrossi@gmail.com>
2025-06-10 08:53:07 -0300

    refactor: remane Encrypter to Hasher

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,6 +1,6 @@
 export * from "../../../domain/use-cases/add-account"
 export * from "../../../domain/models/account"
-export * from "../../protocols/cryptography/encrypter"
+export * from "../../protocols/cryptography/hasher"
 export * from "../../protocols/db/add-account-repository"
 export * from "../../protocols/db/load-account-by-email-repository"
 export * from "../../protocols/db/log-error-repository"
--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,16 +1,16 @@
-import { AccountModel, AddAccount, Encrypter, AddAccountModel, AddAccountRepository } from "./db-add-account-protocols"
+import { AccountModel, AddAccount, Hasher, AddAccountModel, AddAccountRepository } from "./db-add-account-protocols"
 
 export class DbAddAccount implements AddAccount {
-  private readonly encrypter: Encrypter
+  private readonly Hasher: Hasher
   private readonly addAccountRepository: AddAccountRepository
 
 
-  constructor(encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
-    this.encrypter = encrypter
+  constructor(Hasher: Hasher, addAccountRepository: AddAccountRepository) {
+    this.Hasher = Hasher
     this.addAccountRepository = addAccountRepository
   }
   async add(accountData: AddAccountModel): Promise<AccountModel> {
-    const hashedpassword = await this.encrypter.encrypt(accountData.password)
+    const hashedpassword = await this.Hasher.hash(accountData.password)
     const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedpassword }))
     return new Promise(resolve => resolve(account))
   }
