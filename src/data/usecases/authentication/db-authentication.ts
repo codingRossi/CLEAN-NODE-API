b36b761d00ceb2d7399938aa1aa1bd2043660dee
codingrossi <codingrossi@gmail.com>
2025-06-10 08:31:42 -0300

    feat: ensure DbAuthentication calls UpdateAccessTokenRepository with correct values

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -2,15 +2,19 @@ import { Authentication, AuthenticationModel } from "../../../domain/use-cases/a
 import { LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email-repository";
 import { HashComparer } from "../../protocols/cryptography/hash-compare";
 import { TokenGenerator } from "../../protocols/cryptography/token-generator";
+import { UpdateAccessTokenRepository } from "../../protocols/db/update-access-token-repository";
 
 export class DbAuthentication implements Authentication {
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository;
     private readonly hashCompare: HashComparer;
-    tokenGenerator: TokenGenerator
-    constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository, hashCompare: HashComparer, tokenGenerator: TokenGenerator) {
+    private readonly tokenGenerator: TokenGenerator
+    private readonly updateAccessTokenRepository: UpdateAccessTokenRepository;
+
+    constructor(loadAccountByEmailRepository: LoadAccountByEmailRepository, hashCompare: HashComparer, tokenGenerator: TokenGenerator, updateAccessTokenRepository: UpdateAccessTokenRepository) {
         this.tokenGenerator = tokenGenerator;
         this.loadAccountByEmailRepository = loadAccountByEmailRepository;
-        this.hashCompare = hashCompare
+        this.hashCompare = hashCompare;
+        this.updateAccessTokenRepository = updateAccessTokenRepository;
     }
     async auth (authentication: AuthenticationModel): Promise<string> {
         const account = await this.loadAccountByEmailRepository.load(authentication.email);
@@ -18,6 +22,7 @@ export class DbAuthentication implements Authentication {
             const isValid = await this.hashCompare.compare(authentication.password, account.password);
             if (isValid) {
                 const accessToken = await this.tokenGenerator.generate(account.id)
+                await this.updateAccessTokenRepository.update(account.id, accessToken);
                 return accessToken;
             }
         }
