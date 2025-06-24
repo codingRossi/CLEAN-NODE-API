972316ac40d49c0981f8f9161164260df313cf80
codingrossi <codingrossi@gmail.com>
2025-06-24 19:21:05 -0300

    test: ensure SurveyMongoRepository add a new survey on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,41 @@
+import { Collection } from "mongodb"
+import { MongoHelper } from "../helpers/mongo-helper"
+import { SurveyMongoRepository } from "./survey-mongo-repository"
+
+let surveyColletion: Collection
+
+describe("Survey Mongo Repository", () => {
+    beforeAll(async () => {
+        surveyColletion = await MongoHelper.getColletion('survey')
+        await MongoHelper.connect(process.env.MONGO_URL!)
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    beforeEach(async () => {
+        await surveyColletion.deleteMany({})
+    })
+
+    const makeSut = (): SurveyMongoRepository => {
+        return new SurveyMongoRepository()
+    }
+
+    test("Should add a survey on success", async () => {
+        const sut = makeSut()
+        await sut.add({
+            question: "any_question",
+            answers: [{
+                answer: "any_question",
+                image: "any_image"
+            }, {
+                answer: "other_answer",
+
+            }]
+        })
+
+        const survey = await surveyColletion.findOne({ question: "any_question" })
+        expect(survey).toBeTruthy()
+    })
+})
