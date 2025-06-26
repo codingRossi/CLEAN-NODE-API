c24af36ac76a82f8973f49f1b9a6284e177e9888
codingrossi <codingrossi@gmail.com>
2025-06-26 18:51:11 -0300

    fix: mock date before test

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -1,6 +1,7 @@
 import { SurveyModel } from "../../../domain/models/survey"
 import { LoadSurveysRepository } from "../../protocols/db/survey/load-survey-repository"
 import { DbLoadSurveys } from "./db-load-survey"
+import Mockdate from "mockdate"
 
 const makeFakeSurveys = (): SurveyModel[] => {
     return [{
@@ -47,6 +48,13 @@ const makeSut = (): SutTypes => {
     }
 }
 describe("DbLoadSurveys", () => {
+    beforeEach(() => {
+        Mockdate.set(new Date())
+    })
+
+    afterAll(() => {
+        Mockdate.reset()
+    })
     test("Should call LoadSurveysRepository", async () => {
         const { sut, loadSurveysRepositoryStub } = makeSut()
         const loadAllSpy = jest.spyOn(loadSurveysRepositoryStub, "loadAll")
