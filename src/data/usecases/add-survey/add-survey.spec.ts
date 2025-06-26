dc63bd21d753b14547c4807be6c9d85dfd05754c
codingrossi <codingrossi@gmail.com>
2025-06-26 14:27:09 -0300

    test: ensure LoadSurveyesController calls LoadSurveyUseCase

--- src/data/usecases/add-survey/add-survey.spec.ts
+++ src/data/usecases/add-survey/add-survey.spec.ts
@@ -38,7 +38,7 @@ describe("DbAddSurvey UseCase", () => {
         MockDate.set(new Date())
     })
 
-    beforeAll(() => {
+    afterAll(() => {
         MockDate.reset()
     })
     test("Should call AddSurveyRepository with correct values", async () => {
