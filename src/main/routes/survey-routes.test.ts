1b02638cf4cca397abbf1f335f71d31aa38ea3b2
codingrossi <codingrossi@gmail.com>
2025-06-26 18:30:24 -0300

    feat: ensure LoadSurveysRoutes returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -15,18 +15,28 @@ describe("POST /surveys", () => {
     const surveyColletion = await MongoHelper.getColletion('survey')
     await surveyColletion.deleteMany({})
   })
-  test("Should return 403 on add survey without accessToken", async () => {
-    await request(app)
-      .post('/api/surveys')
-      .send({
-        question: "Question",
-        answers: [{
-          answer: "Answer 1",
-          image: "http://image-name.com"
-        }, {
-          answer: "Answer 2"
-        }],
-      })
-      .expect(403)
+  describe("POST /surveys", () => {
+    test("Should return 403 on add survey without accessToken", async () => {
+      await request(app)
+        .post('/api/surveys')
+        .send({
+          question: "Question",
+          answers: [{
+            answer: "Answer 1",
+            image: "http://image-name.com"
+          }, {
+            answer: "Answer 2"
+          }],
+        })
+        .expect(403)
+    })
+  })
+
+  describe("GET /surveys", () => {
+    test("Should return 403 on load survey without accessToken", async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403)
+    })
   })
 })
