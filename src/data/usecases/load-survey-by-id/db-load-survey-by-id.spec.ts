356399b430318613050e7ddd495f12b98a5d6762
codingrossi <codingrossi@gmail.com>
2025-06-27 16:14:13 -0300

    test: ensure DbLoadSurveyById call LoadSurveyByIdRepository with correct values

---
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -0,0 +1,63 @@
+import { SurveyModel } from "@/domain/models/survey"
+import { LoadSurveyByIdRepository } from "@/data/protocols/db/survey/load-survey-by-id-repository"
+import Mockdate from "mockdate"
+import { DbLoadSurveyById } from "./db-load-survey-by-id"
+
+const makeFakeSurvey = (): SurveyModel => {
+    return {
+        id: "any_id",
+        question: "any_question",
+        answers: [{
+            image: "any_image",
+            answer: "any_answer"
+        }],
+        date: new Date()
+    }
+}
+
+type SutTypes = {
+    sut: DbLoadSurveyById
+    loadSurveysByIdRepositoryStub: LoadSurveyByIdRepository
+}
+
+const makeLoadSurveyByIdRepository = (): LoadSurveyByIdRepository => {
+    class LoadSurveysByIdRepositoryStub implements LoadSurveyByIdRepository {
+        async loadById(): Promise<SurveyModel> {
+            return new Promise(resolve => resolve(makeFakeSurvey()))
+        }
+    }
+    return new LoadSurveysByIdRepositoryStub()
+
+}
+
+const makeSut = (): SutTypes => {
+    const loadSurveysByIdRepositoryStub = makeLoadSurveyByIdRepository()
+    const sut = new DbLoadSurveyById(loadSurveysByIdRepositoryStub)
+    return {
+        sut,
+        loadSurveysByIdRepositoryStub
+    }
+}
+
+describe("DbLoadSurveysById", () => {
+    beforeEach(() => {
+        Mockdate.set(new Date())
+    })
+
+    afterAll(() => {
+        Mockdate.reset()
+    })
+    test("Should call LoadSurveysByIdRepository", async () => {
+        const { sut, loadSurveysByIdRepositoryStub } = makeSut()
+        const loadByIdSpy = jest.spyOn(loadSurveysByIdRepositoryStub, "loadById")
+        await sut.loadById("any_id")
+        expect(loadByIdSpy).toHaveBeenCalledWith("any_id")
+    })
+
+    test("Should call LoadSurveysByIdRepository", async () => {
+        const { sut, loadSurveysByIdRepositoryStub } = makeSut()
+        const loadByIdSpy = jest.spyOn(loadSurveysByIdRepositoryStub, "loadById")
+        await sut.loadById("any_id")
+        expect(loadByIdSpy).toHaveBeenCalledWith("any_id")
+    })
+})
